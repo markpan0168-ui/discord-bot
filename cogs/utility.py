53c68c3d92c8c921519346fe8de676a368ebe065
Mark Anthony <markpan0168@gmail.com>
2026-06-06 21:23:00 +0800

    debuggggggg

--- cogs/utility.py
+++ cogs/utility.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from discord.ext import commands
 from discord import app_commands
 
-from config import *
+from cogs.config import *
 
 from cogs.config import MOD_ROLE_ID, suspend_cache
 
